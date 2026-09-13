596ba78438dc9d7a7ff92c5166d639c1fb30a113
Entregador <entregador@users.noreply.github.com>
2026-09-13 23:48:20 +0000

    Atualiza a atividade

--- folhas.js
+++ folhas.js
@@ -106,7 +106,7 @@ function f0(d){
   c.innerHTML =
     '<div class="ceu"><i class="nv n1"></i><i class="nv n2"></i><i class="nv n3"></i></div>' +
     '<h1 class="titu">' + letras + '</h1>' +
-    '<div class="sub">Alfabetização &middot; 1º ano &middot; dez folhas do alfabeto</div>' +
+    '<div class="sub">Alfabetização &middot; 1º ano &middot; vinte e cinco folhas do alfabeto</div>' +
     '<div class="esteira">' +
       '<div class="cena">' + cena + '</div>' +
       '<div class="cinta"><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i></div>' +
